e89a1337b76945f4a98be52aa8befcb10728c386
John <jonnygovish@gmail.com>
2018-06-25 01:56:06 +0300

    redeployment 10

--- deploy.sh
+++ deploy.sh
@@ -100,11 +100,6 @@ selectNodeVersion () {
 
 echo Handling node.js deployment.
 
-# 1. KuduSync
-if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE/dist/" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
-  exitWithMessageOnError "Kudu Sync failed"
-fi
 
 # 2. Select node version
 selectNodeVersion
@@ -128,6 +123,7 @@ echo Handling Angular build
     # IF !ERRORLEVEL! NEQ 0 goto error
     # popd
   fi
+
 
 ##################################################################################################################################
 echo "Finished successfully."
